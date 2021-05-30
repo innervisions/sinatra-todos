591a33a6d757923b48c0b46e07102e4ba1bce88e
Raymond Saade <raymond.saade@gmail.com>
2021-05-29 23:13:03 -0400

    housekeeping

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
+gem "erubis"
 gem "sinatra", "~>1.4.7"
 gem "sinatra-contrib"
-gem "erubis"
--- todo.rb
+++ todo.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+set :port, 8080
 
 get "/" do
   erb "You have no lists.", layout: :layout

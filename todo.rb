1d77cce24aa6ab213d7fa51807ac4caab2374a6c
Raymond Saade <raymond.saade@gmail.com>
2021-06-13 22:07:23 -0400

    added todo ids

--- todo.rb
+++ todo.rb
@@ -37,11 +37,11 @@ helpers do
   end
 
   def sort_todos(todos)
-    todos.each_with_index do |todo, idx|
-      yield(todo, idx) unless todo[:completed]
+    todos.each do |todo|
+      yield(todo) unless todo[:completed]
     end
-    todos.each_with_index do |todo, idx|
-      yield(todo, idx) if todo[:completed]
+    todos.each do |todo|
+      yield(todo) if todo[:completed]
     end
   end
 end
@@ -143,6 +143,11 @@ def error_for_todo(name)
   "Todo must be between 1 and 100 characters."
 end
 
+def next_todo_id(todos)
+  max = todos.map { |todo| todo[:id] }.max || 0
+  max + 1
+end
+
 # Adds a todo to a list.
 post "/lists/:list_id/todos" do
   @list_id = params[:list_id].to_i
@@ -153,7 +158,8 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list
   else
-    @list[:todos] << { name: text, completed: false }
+    id = next_todo_id(@list[:todos])
+    @list[:todos] << { id: id, name: text, completed: false }
     session[:success] = "The todo was added."
     redirect "/lists/#{@list_id}"
   end
@@ -164,7 +170,7 @@ post "/lists/:list_id/todos/:todo_id/destroy" do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
   todo_id = params[:todo_id].to_i
-  @list[:todos].delete_at(todo_id)
+  @list[:todos].reject! { |todo| todo[:id] == todo_id }
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
   else
@@ -179,7 +185,8 @@ post "/lists/:list_id/todos/:todo_id" do
   @list = load_list(@list_id)
   todo_id = params[:todo_id].to_i
   is_completed = (params[:completed] == "true")
-  @list[:todos][todo_id][:completed] = is_completed
+  target = @list[:todos].find { |todo| todo[:id] == todo_id }
+  target[:completed] = is_completed
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
 end
